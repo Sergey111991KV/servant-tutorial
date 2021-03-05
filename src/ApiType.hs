24e00940c4b465ef4b1214b2cc6cd6dd56198e8a
Sergey Kosilov <sergey.cosilov@yandex.ru>
2021-03-05 20:15:31 +0300

    add gitignore file

--- src/ApiType.hs
+++ src/ApiType.hs
@@ -3,8 +3,17 @@
 
 module ApiType where
 
-import Data.Text
-import Data.Time (UTCTime)
-import Servant.API
+-- import Data.Text
+-- import Data.Time (UTCTime)
+-- import Servant.API
 
-type UserAPI = P | PP
+-- type UserAPI = "users" :> QueryParam "sortby" SortBy :> Get '[JSON] [User]
+
+-- data SortBy = Age | Name
+
+-- data User = User {
+--   name :: String,
+--   age :: Int,
+--   email :: String,
+--   registration_date :: UTCTime
+-- }
